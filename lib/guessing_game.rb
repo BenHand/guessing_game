a306d20ad3eae63d19bdf215044d7b1c3cf42692
Benjamin Hand <gneeshot@gmail.com>
2015-05-20 20:07:52 -0500

    game functions, added in array of responses

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -16,20 +16,37 @@ class Guess
 
   def high_or_low?
     if @guess == @number
-      puts "Nice guess. You won with #{@guess_count} attempts!"
+      puts "\nNice guess. You won with #{@guess_count} attempts!\n"
+      exit
     end
     if @guess < @number
-      puts "Try to think BIGGER"
+      puts "\nTry to think BIGGER\n"
     end
     if @guess > @number
-      puts "Whoaa now thats a bit high"
+      puts "\nWhoaa now, thats a bit high\n"
     end
   end
 
   def user_guess
-    "What # am i thinking of? (1-100)? >> "
+    @responses = [
+      "\nWhat # am i thinking of? (1-100)\n",
+      "\nLets try that again..\n",
+      "\nYour not very good at this are you? \n",
+      "\nYou might as well control+c now.\n",
+      "\n...I'll give you one last attempt to redeem yourself.\n"]
+
+    puts @responses[@guess_count]
     @guess = gets.chomp.to_i
     @guess_count += 1
   end
+
+  # def guess_response
+  #   @responses = [
+  #     "What # am i thinking of? (1-100)",
+  #     "Lets try that again.. >> ",
+  #     "Your not very good at this are you? >> ",
+  #     "You might as well control+c now. >> ",
+  #     "...I'll give you one last attempt to redeem yourself. >> "]
+  # end
 Guess.new.start
 end
