8bab82518c4cb2b7c248f39310fcadca734bf67c
Benjamin Hand <gneeshot@gmail.com>
2015-05-20 20:32:09 -0500

    witty response to duplicate number, some refactoring

---
+++ bin/guessing_game.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/guessing_game'
+
+Guess.new.start
--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -3,27 +3,30 @@ class Guess
   def initialize
     @range = (1..100).to_a
     @guess_count = 0
+    @guess_list = []
   end
 
   def start
     @number = @range.sample
     until @guess_count == 5
-      puts @number
       user_guess
       high_or_low?
     end
+    puts "\n Clearly #'s are not your forte.\n #{@number} would have been a good guess."
   end
 
   def high_or_low?
-    if @guess == @number
-      puts "\nNice guess. You won with #{@guess_count} attempts!\n"
-      exit
-    end
-    if @guess < @number
-      puts "\nTry to think BIGGER\n"
-    end
-    if @guess > @number
-      puts "\nWhoaa now, thats a bit high\n"
+    if @guess_count <= 4
+      if @guess == @number
+        puts "\nLucky guess. You won with #{@guess_count} attempts!\n"
+        exit
+      end
+      if @guess < @number
+        puts "\nTry to think BIGGER\n"
+      end
+      if @guess > @number
+        puts "\nWhoaa now, thats a bit high\n"
+      end
     end
   end
 
@@ -31,13 +34,22 @@ class Guess
     @responses = [
       "\nWhat # am i thinking of? (1-100)\n",
       "\nLets try that again..\n",
-      "\nYour not very good at this are you? \n",
+      "\nYour not very good at this, are you? \n",
       "\nYou might as well control+c now.\n",
-      "\n...I'll give you one last attempt to redeem yourself.\n"]
-
+      "\n...I'll give you one last attempt to redeem yourself....\n"]
     puts @responses[@guess_count]
     @guess = gets.chomp.to_i
-    @guess_count += 1
+
+      if @guess_list.include?(@guess)
+        puts "| ~ | ~ " * 30
+         puts "Clearly the stress is just too much.. you already tried #{@guess}. Lets try this again."
+         user_guess
+      else
+        @guess_list << @guess
+
+        @guess_count += 1
+      end
+
   end
 
   # def guess_response
@@ -48,5 +60,4 @@ class Guess
   #     "You might as well control+c now. >> ",
   #     "...I'll give you one last attempt to redeem yourself. >> "]
   # end
-Guess.new.start
 end
